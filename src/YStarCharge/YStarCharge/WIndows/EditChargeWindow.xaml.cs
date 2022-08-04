a061b4c317afe1500c78939a34a1c12d7ebf1f67
15201231344 <15201231344@163.com>
2022-08-04 17:30:45 +0800

    修改主界面和添加编辑界面

---
+++ src/YStarCharge/YStarCharge/WIndows/EditChargeWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YStarCharge.Windows
+{
+    /// <summary>
+    /// AddChargeWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class EditChargeWindow : Window
+    {
+        public EditChargeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DragThumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            double nTop = e.VerticalChange;
+            double nLeft = e.HorizontalChange;
+
+            Top += nTop;
+            Left += nLeft;
+        }
+    }
+}
