74b1ac7ed4a4d9448a0c709741a93a3316078760
15201231344 <15201231344@163.com>
2022-08-10 18:00:10 +0800

    修改支出界面UI

--- src/YStarCharge/YStarCharge/Document/ExpendAndIncomeDocument.xaml.cs
+++ src/YStarCharge/YStarCharge/Document/ExpendAndIncomeDocument.xaml.cs
@@ -18,39 +18,14 @@ namespace YStarCharge.Document
     /// ExpendAndIncomeDocument.xaml 的交互逻辑
     /// </summary>
     public partial class ExpendAndIncomeDocument : UserControl
-    {
-        private readonly Color SelectedColor = Color.FromRgb(91, 150, 219);
-        private readonly Color DefaultColor = Color.FromRgb(177,189,203);
-        private ExpendUserControl expendUserControl = new ExpendUserControl();
+    { 
         public ExpendAndIncomeDocument()
         {
             InitializeComponent();
-        }
 
-        private void ExpendButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                button.Foreground = new SolidColorBrush(SelectedColor);
-            }
-            incomeButton.Foreground = new SolidColorBrush(DefaultColor);
-
-            if (contentGrid.Children.Contains(expendUserControl))
-            {
-                return;
-            }
+            ExpendUserControl euc = new ExpendUserControl();
             contentGrid.Children.Clear();
-            contentGrid.Children.Add(expendUserControl);
-        }
-
-        private void IncomeButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sender is Button button)
-            {
-                button.Foreground = new SolidColorBrush(SelectedColor);
-            }
-            expendButton.Foreground = new SolidColorBrush(DefaultColor);
-
+            contentGrid.Children.Add(euc);
         }
     }
 }
