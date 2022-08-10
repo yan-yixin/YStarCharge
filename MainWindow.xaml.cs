6bff3dbea005040c0918010c2188b0dc0593cfbc
yan-yixin <65119779+yan-yixin@users.noreply.github.com>
2022-08-10 10:39:19 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using YStarCharge.ViewModel;
+
+namespace YStarCharge
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
+            viewModel.ContentGrid = contenGrid;
+
+
+        }
+    }
+}
