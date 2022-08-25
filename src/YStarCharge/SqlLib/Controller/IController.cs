cb47f6f796598578feb719862c0713d46daa9e39
15201231344 <15201231344@163.com>
2022-08-25 18:13:41 +0800

    添加SqlLib类库

---
+++ src/YStarCharge/SqlLib/Controller/IController.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace SqlLib.Controller
+{
+    public interface IController
+    {
+        int Add(IEntity entity);
+
+        int Update(IEntity entity);
+
+        int Delete(IEntity entity);
+
+        IEntity Query(int id);
+
+        DataTable Query();
+    }
+}
