9c9a8e83dc79e9187345bc906e9fb3254852528c
15201231344 <15201231344@163.com>
2024-08-27 21:06:47 +0800

    添加 StatisticsType枚举

---
+++ src/YStarCharge/YStarCharge.Web/Models/Enums/StatisticsType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace YStarCharge.Web.Models.Enums
+{
+    [Description("图表类型")]
+    public enum StatisticsType
+    {
+        [Description("饼形图")]
+        PieChart,
+        [Description("柱状图")]
+        BarChart,
+        [Description("折线图")]
+        LineChart,
+    }
+}
