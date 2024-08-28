70f4af11e38b8da1b7775f6ea6439c7ddba63ea1
15201231344 <15201231344@163.com>
2024-08-28 22:22:48 +0800

    添加登录和注册页面

---
+++ src/YStarCharge/YStarCharge.Web/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YStarCharge.Web.Models;
+
+namespace YStarCharge.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserAccount userAccount = new UserAccount()
+            {
+                Id = 1,
+                Username = "admin",
+                Password = "admin",
+                Icon = "./images/user.png"
+            };
+            return View(userAccount);
+        }
+
+        public IActionResult RemeberPassword() 
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
--- src/YStarCharge/YStarCharge.Web/Models/UserAccount.cs
+++ src/YStarCharge/YStarCharge.Web/Models/UserAccount.cs
@@ -2,5 +2,13 @@
 {
     public class UserAccount
     {
+        public int Id { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Icon { get; set; }
+
     }
 }
